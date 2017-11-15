ea461091cdc278d17c7009db667c7138b6efbd06
Bruno Thiao-Layel <bruno.thiao-layel@c-napps.com>
2017-11-15 15:44:30 +0100

    OK implémentation de 'getPatientById'

--- src/main/java/io/swagger/api/impl/GetPatientApiServiceImpl.java
+++ src/main/java/io/swagger/api/impl/GetPatientApiServiceImpl.java
@@ -13,14 +13,32 @@ import java.io.InputStream;
 
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 import javax.validation.constraints.*;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2017-11-15T14:05:45.222Z")
 public class GetPatientApiServiceImpl extends GetPatientApiService {
-    @Override
-    public Response getPatient(RequestPatientById body, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
-    }
+	@Override
+	public Response getPatient(RequestPatientById body, SecurityContext securityContext) throws NotFoundException {
+		try {
+			Patient p = new Patient()
+					.nom("THIAO")
+					.prenom("BRUNO")
+					.idPatient(body.getIdPatient());
+
+			ObjectMapper om = new ObjectMapper();
+			String jsonResult = om.writeValueAsString(p);
+
+			return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, jsonResult))
+					.build();
+
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "ERROR!"))
+				.build();
+	}
 }
